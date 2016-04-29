8bb4f871aca99e80b4fe1efc4c7143a12adf3bc6
benbiti <wangshouben@hotmail.com>
2016-04-29 14:20:27 +0800

    update test case

--- lzstring-test.lua
+++ lzstring-test.lua
@@ -1,7 +1,8 @@
 -- Test 
 local lzstring = require "lzstring"
 
-print(lzstring.decompress(lzstring.compress('{"v":"growngio.com 数据采集无需埋点 用户行为分析更专业"}')))
-print(lzstring.decompressFromBase64(lzstring.compressToBase64('{"v":"growngio.com 数据采集无需埋点 用户行为分析更专业"}')))
-print(lzstring.decompressFromUTF16(lzstring.compressToUTF16('{"v":"growngio.com 数据采集无需埋点 用户行为分析更专业"}')))
-print(lzstring.decompressFromEncodedURIComponent(lzstring.compressToEncodeURIComponent('{"v":"growngio.com 数据采集无需埋点 用户行为分析更专业"}')))
+print(lzstring.decompress(lzstring.compress('{"v":"growingio.com 数据采集无需埋点 用户行为分析更专业"}')))
+print(lzstring.decompressFromBase64(lzstring.compressToBase64('{"v":"growingio.com 数据采集无需埋点 用户行为分析更专业"}')))
+print(lzstring.decompressFromUTF16(lzstring.compressToUTF16('{"v":"growingio.com 数据采集无需埋点 用户行为分析更专业"}')))
+print(lzstring.decompressFromEncodedURIComponent(lzstring.compressToEncodeURIComponent('{"v":"growingio.com 数据采集无需埋点 用户行为分析更专业"}')))
+print(lzstring.decompressFromEncodedURIComponent(lzstring.compressToEncodeURIComponent('AAAAABBABCABCABC')))
